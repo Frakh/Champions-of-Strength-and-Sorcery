f310ab78afc3901f88ae0e0874eb8de9d961d730
MakoPlatinum <couturier.paul75@gmail.com>
2017-01-23 16:49:45 +0100

    javadoc légèrement retravaillé

--- src/es/interfaces/IControllable.java
+++ src/es/interfaces/IControllable.java
@@ -2,6 +2,10 @@ package es.interfaces;
 
 public interface IControllable {
 
+	/**
+	 * Methode d'obtention du controlleur
+	 * @return Le controlleur associé
+	 */
 	IController getController();
 
 }
