72eb542d8a0bc48c5313aec75c52cd3574761819
Pol-AB <polaurelien.boidot@gmail.com>
2017-01-30 16:10:28 +0100

    Vous puez la merde appren,ez à jouer cqvssrtdt-tt

--- src/game/carte/Case.java
+++ src/game/carte/Case.java
@@ -1,31 +1,4 @@
 package game.carte;
 
-public enum Case implements es.interfaces.ISpriteDrawable{
-	Herbe("",1.0),
-	Terre("",1.0),
-	Lave("",1.0),
-	Souterrain("",1.0),
-	Roche("",0.0),
-	Toundra("",1.25),
-	Sable("",1.5),
-	Neige("",1.5),
-	Marais("",1.75),
-	Eau("",0.0);
-
-	private String sprite = "";
-	private double mvtCost = 0;
-	
-	Case(String spritePath, double cost){
-		this.sprite = spritePath;
-		this.mvtCost = cost;
-	}
-	
-	public double getMvtCost() {
-		return mvtCost;
-	}
-	
-	@Override
-	public String getImage() {
-		return sprite;
-	}
+public enum Case {
 }
