62ec71954a40fc7c89ed06fd500fb455dce9113b
CfwIA <23635611+CfwIA@users.noreply.github.com>
2017-02-20 14:36:37 +0100

    Commit de la carte

--- src/game/carte/Carte.java
+++ src/game/carte/Carte.java
@@ -1,45 +1,32 @@
 package game.carte;
 
-import utilitaire.IPosition;
 import es.exception.IllegalFriendException;
 import es.sortie.composants.CarteLayer;
 import es.sortie.composants.ObjetLayer;
-import utilitaire.Position;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 
 public class Carte {
 	private Case[][] sol;
-	private Map<IPosition,IElement> elements;
+	private IElement[][] elements;
 
 	public Carte(Case[][] sol) {
 		this.sol = sol;
 	}
 
-	public Carte(Case[][] sol, Map<IPosition, IElement> elements) {
+	public Carte(Case[][] sol, IElement[][] elements) {
+		// Copie de pointers
 		this.sol = sol;
 		this.elements = elements;
 	}
 	
 	//le constructeur du debug wesh
-	public Carte(int largeur, int hauteur, Case[][] sol, HashMap<IPosition,IElement> elements){
+	public Carte(int largeur, int hauteur, Case[][] sol, IElement[][] elements){
 		sol = new Case[largeur][hauteur];
-		this.sol=sol;
-		Set<IPosition> ip = elements.keySet();
+		if (sol == null || elements==null)
+			throw new IllegalArgumentException("Argument null : " + (sol==null?"sol":"elements"));
 
-		if (elements!=null) {
-			// Verification que toutes les positions soient dans la map
-			for (IPosition p : ip) {
-				// La hauteur, c'est Y, la largeur, c'est X
-				// Si Y augmente, ça descend, si X augmente, ça vas vers la droite
-				if (p.getX() < 0 || p.getY() < 0 || p.getX() > largeur || p.getY() > hauteur)
-					throw new IllegalArgumentException("Une position ne rentre pas dans les dimensions : " + p.toString());
-			}
-			this.elements = elements;
-		}
+		this.sol=sol;
+		this.elements = elements;
 	}
 	
 	//le constructeur du fichier texte
@@ -50,7 +37,7 @@ public class Carte {
 	}
 	
 	public IElement getElement(int x, int y){
-		return elements.get(new Position(x,y));
+		return elements[x][y];
 	}
 
 	public int getHauteur() {
@@ -69,7 +56,7 @@ public class Carte {
 	}
 
 	// friend class ObjetLayer;
-	public Map<IPosition, IElement> getElements(ObjetLayer.Friend friend) {
+	public IElement[][] getElements(ObjetLayer.Friend friend) {
 		if (friend==null)
 			throw new IllegalFriendException();
 		return elements;
