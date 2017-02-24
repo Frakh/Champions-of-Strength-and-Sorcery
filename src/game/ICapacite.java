4909bb0b1533d752b5605728e7cfa51612aa025f
Ereshkigal <gauvain.seron42@gmail.com>
2017-02-24 16:53:25 +0100

    Test

--- src/game/ICapacite.java
+++ src/game/ICapacite.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package game;
 
 public interface ICapacite {
 
 }
-=======
-package game;
-
-public interface ICapacite {
-
-}
->>>>>>> branch 'master' of https://github.com/Frakh/Champions-of-Strength-and-Sorcery.git
