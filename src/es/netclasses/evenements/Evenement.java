e4f13d6ebc9332e9631f334d90bdef2c286c24b1
MakoPlatinum <couturier.paul75@gmail.com>
2017-02-27 14:06:58 +0100

    Modif classe evenement

--- src/es/netclasses/evenements/Evenement.java
+++ src/es/netclasses/evenements/Evenement.java
@@ -2,13 +2,21 @@ package es.netclasses.evenements;
 
 public class Evenement {
 
-	private int eventId;
+	private short eventId;
 
-	public Evenement(int eventId) {
+	public Evenement(short eventId) {
 		this.eventId = eventId;
 	}
 
-	public int getEventId() {
+	public int getFullEventId() {
 		return eventId;
+	}
+
+	public int getId() {
+		return eventId*(eventId<0?-1:1);
+	}
+
+	public boolean isDispachtable() {
+		return eventId<0;
 	}
 }
