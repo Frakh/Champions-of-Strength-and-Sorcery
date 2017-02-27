8a96b20c9df4eec8ab94f5a47ee9d26ec3e1fbcc
kratolos <chifflot@B-1-17-06.iutparis.local>
2017-02-27 18:00:51 +0100

    commit message

--- src/game/Jeu.java
+++ src/game/Jeu.java
@@ -1,4 +1,14 @@
 package game;
 
+import game.carte.Carte;
+
 public class Jeu {
+	private Carte surface, souterrain;
+	private Joueur[] joueurs;
+	
+	public Jeu (Carte surface, Carte souterrain, Joueur[] joueurs) {
+		this.surface = surface;
+		this.souterrain = souterrain;
+		this.joueurs = joueurs;
+	}
 }
