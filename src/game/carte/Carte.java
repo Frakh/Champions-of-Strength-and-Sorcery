44d49472082ae91110a8442aab9d36c49868e909
kratolos <chifflot@B-1-17-06.iutparis.local>
2017-03-06 14:27:38 +0100

    gé réparai la cart lol

--- src/game/carte/Carte.java
+++ src/game/carte/Carte.java
@@ -3,7 +3,9 @@ package game.carte;
 import es.exception.IllegalFriendException;
 import es.sortie.composants.CarteLayer;
 import es.sortie.composants.ObjetLayer;
+import game.carte.elements.HerosMap;
 import utilitaire.IPosition;
+import utilitaire.Vector2i;
 
 
 public class Carte {
@@ -33,13 +35,13 @@ public class Carte {
 	//le constructeur du fichier texte
 	//public Carte(String path) mais j'ai la flemme pour l'instant
 	
-	public boolean canMove(IPosition pos){
-		if (sol[(int) pos.getX()][(int) pos.getY()].getMvtCost()==0) return false;
+	public boolean canMove(Vector2i pos){
+		if (sol[pos.getX()][pos.getY()].getMvtCost()==0) return false;
 		return true;
 	}
 	
 	public Case getCase(int x, int y){
-		return sol[y][x];
+		return sol[x][y];
 	}
 	
 	public IElement getElement(int x, int y){
@@ -66,5 +68,82 @@ public class Carte {
 		if (friend==null)
 			throw new IllegalFriendException();
 		return elements;
+	}
+	
+	public IElement checkElement(int x, int y){
+		 if (elements[x][y]!=null) return elements[x][y];
+		 	for (int i=x-1;i<=x+1;++i){
+		 		for (int j=y-1;j<=y+1;++j){
+		 			if(i!=x&&j!=y){
+		 				if (elements[i][j]!=null) return elements[i][j];
+		 			}
+		 		}
+		 	}
+		 	return null;
+		}
+	
+	public PFUtil[][] pathfinding(int coordHerosH, int coordHerosL, int nbPointsMouv){ // renvoie un tableau de bools, représentant les cases accessibles par un monstre qui marche par terre
+		// case non visitée = -1
+		// case accessible = + de 0
+		int k;
+		int l;
+		PFUtil trucARetourner[][] = new PFUtil[getHauteur()][getLargeur()];
+		int truc[][]=new int[getHauteur()][getLargeur()]; // ai besoin de truc pour avoir faux, vrai et à rendre vrai
+		for(int i=0; i<getHauteur(); i++){
+			for (int j=0; j<getLargeur(); j++){
+				trucARetourner[i][j].setCoord(-1);
+				truc[i][j]=0;
+			}
+		}
+		trucARetourner[coordHerosH][coordHerosL].setCoord(0);
+		int mouvement = 1000;// = case[coordHerosH][coordHerosL].getHeros.getMouvement;
+		boolean Continue=true;
+		while (Continue){//while for for if if while if while if. bon appétit bien sûr.
+			for(int i=0; i<getHauteur(); i++){
+				for (int j=0; j<getLargeur(); j++){
+					if (trucARetourner[coordHerosH][coordHerosL].getCoord()!=-1 && !trucARetourner[coordHerosH][coordHerosL].isEvent()){
+						k=i-1;
+						if (k<0) {k=0;}
+						while (k <= i+1 && k<getHauteur()){//double boucle while parce que for me faisait sortir du tableau
+							l=j-1;
+							if (l<0) {l=0;}
+							while (l <= j+1 && l<getLargeur()){	
+								if (canMove(new Vector2i(k,l)) && (trucARetourner[k][l].getCoord()<trucARetourner[i][j].getCoord()+sol[k][l].getMvtCost())&&(trucARetourner[i][j].getCoord()+sol[k][l].getMvtCost()<mouvement)){
+									trucARetourner[k][l].setCoord(trucARetourner[i][j].getCoord()+sol[k][l].getMvtCost());
+									if (checkElement(k,l)!=null)
+										trucARetourner[k][l].setEvent(true);
+								}
+								l++;
+							}
+							k++;
+						}
+					}
+				}
+			}
+			Continue=false;
+			for(int i=0; i<getHauteur(); i++){
+				for (int j=0; j<getLargeur(); j++){
+					if (trucARetourner[i][j].getCoord()!=truc[i][j]) Continue = true;
+				}
+			}
+		}
+		return trucARetourner;
+	}
+	
+	public void deplacer(HerosMap heros, int coordHerosH, int coordHerosL, Vector2i pos) throws Exception {
+		PFUtil [][] tabDep = pathfinding(coordHerosH, coordHerosL, heros.getHeros().getPtDeplacement());
+		PFUtil caseCible = tabDep[pos.getX()][pos.getY()];
+		int coutDep = caseCible.getCoord();
+		if(coutDep < 0) {
+			//throw new Exception("impossible de se deplacer la-bas");
+		}
+		else {
+			heros.getHeros().setPtDeplacement(heros.getHeros().getPtDeplacement() - coutDep);
+			elements[pos.getX()][pos.getY()] = heros;
+			elements[coordHerosH][coordHerosL] = null;
+			if(caseCible.isEvent()) {
+				elements[pos.getX()][pos.getY()].interagir(heros.getHeros());
+			}
+		}
 	}
 }
