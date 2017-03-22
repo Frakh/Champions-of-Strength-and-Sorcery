f0a63ea41ea2e7bb27505b3ac8acb0351bbc9173
Mendelrock <Mendelrock@users.noreply.github.com>
2017-03-22 21:29:14 +0100

    J'ai fait des ressources...

--- src/game/carte/elements/Ressources.java
+++ src/game/carte/elements/Ressources.java
@@ -1,15 +1,17 @@
 package game.carte.elements;
 
-import es.interfaces.ISpriteDrawable;
+import game.Heros;
+import game.carte.IElement;
 
-public enum Ressources implements ISpriteDrawable {
+public enum Ressources implements IElement {
 	OR("OR.png"),
 	BOIS("BOIS.png"),
 	MINERAI("MINERAI.png"),
 	MERCURE("MERCURE.png"),
 	SOUFFRE("SOUFFRE.png"),
 	CRISTAUX("CRISTAUX.png"),
-	GEMMES("GEMMES.png");
+	GEMMES("GEMMES.png"),
+	COFFRE("");
 
 	private String sprite;
 	
@@ -20,6 +22,18 @@ public enum Ressources implements ISpriteDrawable {
 	@Override
 	public String getImage() {
 		return "./assets/img/"+sprite;
+	}
+
+	@Override
+	public void interagir(Heros oui) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String decrire() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
