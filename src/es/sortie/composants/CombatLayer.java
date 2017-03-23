4d8c621f34ecdbce24679230f219cb1a04b5360d
MakoPlatinum <couturier.paul75@gmail.com>
2017-03-23 16:06:17 +0100

    Ajout du layer de combat, et autres joyeusetés

---
+++ src/es/sortie/composants/CombatLayer.java
@@ -0,0 +1,51 @@
+package es.sortie.composants;
+
+import es.dataManager.ImageManager;
+import es.sortie.FrameManager;
+import game.combat.Combat;
+
+import java.awt.*;
+
+public class CombatLayer extends AbstractBufferComposant {
+
+	private Combat combat;
+
+	public static String fondCombat = "./assets/img/bg_plaine.jpg";
+
+	public CombatLayer(FrameManager fm, Combat combat) {
+		super(fm, null);
+		this.combat = combat;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+
+		g2.drawImage(
+				ImageManager.getImage(fondCombat),
+				0,0,gdb.winWidth, gdb.winHeight, this	);
+
+		for (int i = 0; i <= 13; ++i) {
+			try {
+				int x = combat.getCoordTroupes(i).x;
+				int y = combat.getCoordTroupes(i).y;
+				String urlimage = combat.terrainCombat[x][y].getUnit().getImage();
+				g2.drawImage(
+						ImageManager.getImage(urlimage),
+						x * gdb.spriteWidth,
+						y * gdb.spriteHeight,
+						gdb.spriteWidth,
+						gdb.spriteHeight,
+						this
+				);
+			} catch (RuntimeException ignored) {
+			}
+		}
+
+	}
+
+	@Override
+	void dessiner(final Graphics g) {
+		paintComponent(g);
+	}
+}
