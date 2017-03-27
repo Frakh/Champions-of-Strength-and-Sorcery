6bcdf4af33ab6f0079297f4ce32fc35a70294179
MakoPlatinum <couturier.paul75@gmail.com>
2017-03-27 11:35:46 +0200

    Modif pour faciliter l'utilisation de es

--- src/es/sortie/composants/InterfaceUtilisateurLayer.java
+++ src/es/sortie/composants/InterfaceUtilisateurLayer.java
@@ -15,12 +15,39 @@ public class InterfaceUtilisateurLayer extends AbstractBufferComposant {
 
 	//La position en pixels des images sur la carte
 	private Map<Vector2i, String> mapPositionImage;
+	private Map<Vector2i, Object> donnes_pos_img;
 
 	/**
 	 * Constructeur de l'UILayer
 	 */
 	public InterfaceUtilisateurLayer() {
 		super(null, null);
+	}
+
+	/**
+	 * Permet de vider les cartes
+	 */
+	public void emptyMaps() {
+		mapPositionImage.clear();
+		donnes_pos_img.clear();
+	}
+
+	/**
+	 * Permet d'ajouter une image sur l'ecran de l'ui
+	 * @param v : la position en pixel
+	 * @param s : l'adresse sur le disque de l'image
+	 */
+	public void ajouterImageUI(Vector2i v, String s) {
+		mapPositionImage.put(v,s);
+	}
+
+	/**
+	 * Permet d'ajouter une chaine a écrire sur l'écran :
+	 * @param v : la position en pixel
+	 * @param o : l'objet ( methode toString appelé )
+	 */
+	public void ajouterDonnesUI(Vector2i v, Object o) {
+		donnes_pos_img.put(v, o);
 	}
 
 	/**
