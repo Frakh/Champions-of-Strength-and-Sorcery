321532d990ed16030b1e4655a5e2e67cd436dabc
MakoPlatinum <couturier.paul75@gmail.com>
2017-03-27 11:55:13 +0200

    Modification du server

--- ChampionServeur/src/es/netclasses/evenements/eventimpl/JeuEvenement.java
+++ ChampionServeur/src/es/netclasses/evenements/eventimpl/JeuEvenement.java
@@ -4,12 +4,16 @@ import es.netclasses.evenements.Evenement;
 
 public class JeuEvenement extends Evenement{
 
-	//Detail a fournir : l'id de la partie
+	//Detail a fournir : l'id de la partie / la map a charger
 	public static final byte JOIN_GAME = 0;
 	//Detail a fournir : le nom de la carte
 	public static final byte CREATE_GAME = 1;
 	//Detail a fournir = aucun / Liste des jeux
 	public static final byte GAME_LIST = 2;
+	//Detail a fournir : ne doit pas être lancé par le client / rien
+	public static final byte NOT_FOUND_GAME = 3;
+	//Detail a fournir : rien / doit être donné par le server a tous le monde
+	public static final byte RUN_GAME = 4;
 
 	private byte messageId;
 	private String detail;
--- ChampionServeur/src/game/Partie.java
+++ ChampionServeur/src/game/Partie.java
@@ -10,10 +10,18 @@ public class Partie extends BaseThread {
 	private static String partieJoinable;
 
 	private Vector<Joueur> joueurs;
+	private Joueur proprietaire;
+	private String map;
 
-	public Partie(String map) {
+	public Partie(String map, Joueur proprietaire) {
 		partieVector.add(this);
+		this.proprietaire = proprietaire;
+		this.map = map;
 		joueurs = new Vector<>();
+	}
+
+	public static void creerPartie(Joueur j, String map) {
+		new Partie(map, j).start();
 	}
 
 	public static String getPartiesJoinable() {
@@ -28,5 +36,13 @@ public class Partie extends BaseThread {
 	@Override
 	public String toString() {
 		return "Nombre de joueurs : " + joueurs.size() + '\n';
+	}
+
+	public static void rejoindre(final int i, final Joueur joueur) {
+		partieVector.get(i).ajouterJoueur(joueur);
+	}
+
+	private void ajouterJoueur(final Joueur joueur) {
+		joueurs.add(joueur);
 	}
 }
