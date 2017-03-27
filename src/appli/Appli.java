f5d220579fb06555e2043d4dc7fb9febb9874a16
Pol-AB <polaurelien.boidot@gmail.com>
2017-03-27 12:16:08 +0200

    début d'appli et création des assets du menu principal

--- src/appli/Appli.java
+++ src/appli/Appli.java
@@ -1,10 +1,45 @@
 package appli;
 
+import es.sortie.FrameManager;
+import es.sortie.composants.AbstractBufferComposant;
+import es.sortie.composants.CarteLayer;
+import es.sortie.composants.InterfaceUtilisateurLayer;
+import utilitaire.IPosition;
+import utilitaire.Position;
+
 public class Appli {
 	public static void main (String[] args){
 		//charger des trucs
 		//lancer cinématique d'intro stylée
 		//lancer menu principal
+		
+		FrameManager fm = new FrameManager();
+		IPosition ip = new Position(0,0);
+
+		fm.setSpriteDim(32, 32);
+		fm.setPositionToFollow(ip);
+		fm.setDimensions(1280,720);
+
+		AbstractBufferComposant menu = new InterfaceUtilisateurLayer();
+
+		fm.init(menu); 
+		
+		/*
+		 là il faut:
+		 -afficher le menu principal
+		 -gérer les clics souris pour sélectionner des options(créer/rejoindre partie, etc)
+		 je sais faire aucun des deux donc bon 
+		*/
+
+		for (int i = 0; i < 10; ++i) {
+			fm.repaint();
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 }
