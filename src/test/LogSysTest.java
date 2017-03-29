1cc3cd2339630ce0a868dd151233317ede7fdfe1
CfwIA <23635611+CfwIA@users.noreply.github.com>
2017-03-30 00:22:46 +0200

    Modif sur le layer de combat

--- src/test/LogSysTest.java
+++ src/test/LogSysTest.java
@@ -11,5 +11,11 @@ public class LogSysTest {
 		System.out.println(LogSys.log("Hello World"));
 		LogSys.exit();
 
+		char a = '4';
+		char b = '1';
+		int v = Integer.parseInt(String.valueOf(a)+b);
+
+		System.out.println(Character.getNumericValue('7'));
+		System.out.println(v);
 	}
 }
