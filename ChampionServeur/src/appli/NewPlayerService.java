1f1c7005ab7dc987346fae3b818d582bf545e599
MakoPlatinum <couturier.paul75@gmail.com>
2017-03-30 11:45:15 +0200

    Modif sur le serveur

--- ChampionServeur/src/appli/NewPlayerService.java
+++ ChampionServeur/src/appli/NewPlayerService.java
@@ -27,25 +27,28 @@ public class NewPlayerService implements Runnable {
 
 		System.out.println("New Player " + "Etablished Connection");
 
-		JeuEvenement jevent = (JeuEvenement) joueur.getSocketFlux().readEvenement();
+		while (true) {
+			JeuEvenement jevent = (JeuEvenement) joueur.getSocketFlux().readEvenement();
 
-		switch (jevent.getMessageId()) {
-			case JeuEvenement.GAME_LIST:
-				joueur.getSocketFlux().writeEvenement(new JeuEvenement(JeuEvenement.GAME_LIST, Partie.getPartiesJoinable()));
-				break;
-			case JeuEvenement.CREATE_GAME:
-				Partie.creerPartie(joueur, jevent.getDetail());
-				return;
-			case JeuEvenement.JOIN_GAME:
-				try {
-					Partie.rejoindre(Integer.parseInt(jevent.getDetail()), joueur);
+			switch (jevent.getMessageId()) {
+				case JeuEvenement.GAME_LIST:
+					joueur.getSocketFlux().writeEvenement(new JeuEvenement(JeuEvenement.GAME_LIST, Partie.getPartiesJoinable()));
+					break;
+				case JeuEvenement.CREATE_GAME:
+					System.out.println("Amorcement creation partie");
+					Partie.creerPartie(joueur, jevent.getDetail());
 					return;
-				} catch (Exception e) {
-					joueur.getSocketFlux().writeEvenement(new JeuEvenement(JeuEvenement.NOT_FOUND_GAME, ""));
-				}
-				break;
-			default:
-				break;
+				case JeuEvenement.JOIN_GAME:
+					try {
+						Partie.rejoindre(Integer.parseInt(jevent.getDetail()), joueur);
+						return;
+					} catch (Exception e) {
+						joueur.getSocketFlux().writeEvenement(new JeuEvenement(JeuEvenement.NOT_FOUND_GAME, ""));
+					}
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
